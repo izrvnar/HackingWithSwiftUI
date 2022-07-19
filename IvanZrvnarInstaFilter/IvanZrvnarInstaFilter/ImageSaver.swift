136d36d6ae950a0e129e87535adb7fd654548c97
izrvnar <72573842+izrvnar@users.noreply.github.com>
2022-07-19 10:42:47 -0400

    Add files via upload

---
+++ IvanZrvnarInstaFilter/IvanZrvnarInstaFilter/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  IvanZrvnarInstaFilter
+//
+//  Created by Ivan Zrvnar on 2022-07-09.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
---
+++ IvanZrvnarInstaFilter/IvanZrvnarInstaFilter/IvanZrvnarInstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  IvanZrvnarInstaFilterApp.swift
+//  IvanZrvnarInstaFilter
+//
+//  Created by Ivan Zrvnar on 2022-07-09.
+//
+
+import SwiftUI
+
+@main
+struct IvanZrvnarInstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
